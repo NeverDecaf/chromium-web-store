2c6a59fe6f6026fdf975c683059e4bcd508b355f
NeverDecaf <jtr9289@gmail.com>
2019-08-18 19:21:10 -0400

    Correctly handles url fragments

--- src/inject.js
+++ src/inject.js
@@ -2,39 +2,54 @@
 var bodyObserver = new MutationObserver(function (mutations) {
 	
 	function getExtensionId(url) {
+		url = url.split('#')[0].split('?')[0];
 		var pathfrags = url.split('/');
 		return pathfrags[pathfrags.length-1];
 	};
 	function buildExtensionUrl(extensionId) {
 		var chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
-		return 'https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion='+chromeVersion+'&x=id%3D'+extensionId+'%26installsource%3Dondemand%26uc'
+		return 'https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion='+chromeVersion+'&x=id%3D'+extensionId+'%26installsource%3Dondemand%26uc';
 	};
 	mutations.forEach(function (mutation) {
 		if (mutation.attributeName == 'class' && mutation.target.tagName == 'DIV' && (mutation.target.className == 'a-eb-mb-x' || mutation.target.hasAttribute('webstore-source'))) {
 		var xpathResult = document.evaluate( '//div[div[@aria-label="Available on Chrome"] or (@class="h-e-f-Ra-c e-f-oh-Md-zb-k" and not(node())) ]', document, null, XPathResult.ANY_TYPE, null);
-
 		var results = [];
 		while (result = xpathResult.iterateNext())
 			results.push(result);
 		
 		for (var i=0; i < results.length; i++) {
-			
-			button_div = document.createElement( 'div' );
+			var button_div = document.createElement( 'div' );
 			button_div.setAttribute('role','button');
 			button_div.setAttribute('class','dd-Va g-c-wb g-eg-ua-Uc-c-za g-c-Oc-td-jb-oa g-c');
 			button_div.setAttribute('aria-label','Add to Chromium');
 			button_div.setAttribute('tabindex','0');
 			button_div.setAttribute('style','user-select: none;');
-
-			hf = document.createElement( 'div' );
+			var dlurl = '';
+			if (parentpanel = results[i].closest('a')) {
+				dlurl = buildExtensionUrl(getExtensionId(parentpanel.href));
+				parentpanel.setAttribute('ext_dl_url',dlurl);
+				parentpanel.addEventListener("click",function(){
+					this.setAttribute('id','ext_dl_url');
+				});
+			}
+			else if (clickedon = document.getElementById('ext_dl_url')) {
+				clickedon.removeAttribute('id');
+				dlurl = clickedon.getAttribute('ext_dl_url');
+			}
+			else {
+				dlurl = buildExtensionUrl(getExtensionId(document.head.querySelector('meta[property="og:url"]').getAttribute('content')));
+			}
+			button_div.setAttribute('ext_dl_url',dlurl);
+			
+			var hf = document.createElement( 'div' );
 			hf.setAttribute('class','g-c-Hf');
 			button_div.appendChild(hf);
 			
-			x = document.createElement( 'div' );
+			var x = document.createElement( 'div' );
 			x.setAttribute('class','g-c-x');
 			hf.appendChild(x);
 			
-			R = document.createElement( 'div' );
+			var R = document.createElement( 'div' );
 			R.setAttribute('class','g-c-R  webstore-test-button-label');
 			R.innerHTML = 'Add to Chromium';
 			x.appendChild(R);
@@ -46,10 +61,7 @@ var bodyObserver = new MutationObserver(function (mutations) {
 				this.classList.remove('g-c-l');
 			});
 			button_div.addEventListener("click",function(){
-				if (this.parentElement.className == 'h-e-f-Ra-c e-f-oh-Md-zb-k')
-					window.open(buildExtensionUrl(getExtensionId(document.location.href)))
-				else
-					window.open(buildExtensionUrl(getExtensionId(this.closest('a').href)));
+				window.open(this.getAttribute('ext_dl_url'));
 			});
 			
 			results[i].innerHTML = "";
